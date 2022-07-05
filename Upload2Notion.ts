cdc0a63ce3d425e76aa07b478c031f2d107a75f9
Oleg <oleg.lustenko@gmail.com>
2022-07-05 17:56:15 +0300

    add property type

--- Upload2Notion.ts
+++ Upload2Notion.ts
@@ -1,8 +1,8 @@
-import { Notice, requestUrl,TFile,normalizePath, App } from "obsidian";
+import { Notice, requestUrl, TFile, normalizePath, App } from "obsidian";
 import { Client } from "@notionhq/client";
-import { markdownToBlocks,  } from "@tryfabric/martian";
+import { markdownToBlocks } from "@tryfabric/martian";
 import * as yamlFrontMatter from "yaml-front-matter";
-import * as yaml from "yaml"
+import * as yaml from "yaml";
 import MyPlugin from "main";
 export class Upload2Notion {
 	app: MyPlugin;
@@ -12,34 +12,35 @@ export class Upload2Notion {
 		this.app = app;
 	}
 
-	async deletePage(notionID:string){	
+	async deletePage(notionID: string) {
 		const response = await requestUrl({
 			url: `https://api.notion.com/v1/blocks/${notionID}`,
-			method: 'DELETE',
+			method: "DELETE",
 			headers: {
-				'Content-Type': 'application/json',
-				'Authorization': 'Bearer ' + this.app.settings.notionAPI,
-				'Notion-Version': '2022-02-22',
+				"Content-Type": "application/json",
+				Authorization: "Bearer " + this.app.settings.notionAPI,
+				"Notion-Version": "2022-02-22",
 			},
-			body: ''
-		})
-		return response;	
+			body: "",
+		});
+		return response;
 	}
 
 	// 因为需要解析notion的block进行对比，非常的麻烦，
 	// 暂时就直接删除，新建一个page
-	async updatePage(notionID:string, title:string, childArr:any) {
-		await this.deletePage(notionID)
-		const res = await this.createPage(title, childArr)
-		return res
+	async updatePage(notionID: string, title: string, childArr: any) {
+		await this.deletePage(notionID);
+		const res = await this.createPage(title, childArr);
+		return res;
 	}
 
-	async createPage(title:string, childArr: any) {
-		const bodyString:any = {
-			parent: { 
-				database_id: this.app.settings.databaseID 
-			},	
+	async createPage(title: string, childArr: any) {
+		const bodyString: any = {
+			parent: {
+				database_id: this.app.settings.databaseID,
+			},
 			properties: {
+				type: "rich_text",
 				Name: {
 					title: [
 						{
@@ -51,87 +52,101 @@ export class Upload2Notion {
 				},
 			},
 			children: childArr,
-		}
+		};
 
-		if(this.app.settings.bannerUrl) {
+		if (this.app.settings.bannerUrl) {
 			bodyString.cover = {
 				type: "external",
 				external: {
-					url: this.app.settings.bannerUrl
-				}
-			}
+					url: this.app.settings.bannerUrl,
+				},
+			};
 		}
 
 		try {
 			const response = await requestUrl({
 				url: `https://api.notion.com/v1/pages`,
-				method: 'POST',
+				method: "POST",
 				headers: {
-					'Content-Type': 'application/json',
+					"Content-Type": "application/json",
 					// 'User-Agent': 'obsidian.md',
-					'Authorization': 'Bearer ' + this.app.settings.notionAPI,
-					'Notion-Version': '2021-08-16',
+					Authorization: "Bearer " + this.app.settings.notionAPI,
+					"Notion-Version": "2021-08-16",
 				},
 				body: JSON.stringify(bodyString),
-			})
+			});
 			return response;
 		} catch (error) {
-				new Notice(`network error ${error}`)
-		}	
+			new Notice(`network error ${error}`);
+		}
 	}
 
-	async syncMarkdownToNotion(title:string, markdown: string, nowFile: TFile, app:App, settings:any): Promise<any> {
-		let res:any
-		const yamlObj:any = yamlFrontMatter.loadFront(markdown);
-		const __content = yamlObj.__content
+	async syncMarkdownToNotion(
+		title: string,
+		markdown: string,
+		nowFile: TFile,
+		app: App,
+		settings: any
+	): Promise<any> {
+		let res: any;
+		const yamlObj: any = yamlFrontMatter.loadFront(markdown);
+		const __content = yamlObj.__content;
 		const file2Block = markdownToBlocks(__content);
-		const frontmasster =await app.metadataCache.getFileCache(nowFile)?.frontmatter
-		const notionID = frontmasster ? frontmasster.notionId : null
+		const frontmasster = await app.metadataCache.getFileCache(nowFile)
+			?.frontmatter;
+		const notionID = frontmasster ? frontmasster.notionId : null;
 
-		if(notionID){
-				res = await this.updatePage(notionID, title, file2Block);
+		if (notionID) {
+			res = await this.updatePage(notionID, title, file2Block);
 		} else {
-			 	res = await this.createPage(title, file2Block);
+			res = await this.createPage(title, file2Block);
 		}
 		if (res.status === 200) {
-			await this.updateYamlInfo(markdown, nowFile, res, app, settings)
+			await this.updateYamlInfo(markdown, nowFile, res, app, settings);
 		} else {
-			new Notice(`${res.text}`)
+			new Notice(`${res.text}`);
 		}
-		return res
+		return res;
 	}
 
-	async updateYamlInfo(yamlContent: string, nowFile: TFile, res: any,app:App, settings:any) {
-		const yamlObj:any = yamlFrontMatter.loadFront(yamlContent);
-		let {url, id} = res.json
-		// replace www to notionID	
-		const {notionID} = settings;
-		if(notionID!=="") {
+	async updateYamlInfo(
+		yamlContent: string,
+		nowFile: TFile,
+		res: any,
+		app: App,
+		settings: any
+	) {
+		const yamlObj: any = yamlFrontMatter.loadFront(yamlContent);
+		let { url, id } = res.json;
+		// replace www to notionID
+		const { notionID } = settings;
+		if (notionID !== "") {
 			// replace url str "www" to notionID
-			url  = url.replace("www.notion.so", `${notionID}.notion.site`)			
+			url = url.replace("www.notion.so", `${notionID}.notion.site`);
 		}
 		yamlObj.link = url;
 		try {
-			await navigator.clipboard.writeText(url)
+			await navigator.clipboard.writeText(url);
 		} catch (error) {
-			new Notice(`复制链接失败，请手动复制${error}`)
+			new Notice(`复制链接失败，请手动复制${error}`);
 		}
 		yamlObj.notionID = id;
 		const __content = yamlObj.__content;
-		delete yamlObj.__content
-		const yamlhead = yaml.stringify(yamlObj)
+		delete yamlObj.__content;
+		const yamlhead = yaml.stringify(yamlObj);
 		//  if yamlhead hava last \n  remove it
-		const yamlhead_remove_n = yamlhead.replace(/\n$/, '')
+		const yamlhead_remove_n = yamlhead.replace(/\n$/, "");
 		// if __content have start \n remove it
-		const __content_remove_n = __content.replace(/^\n/, '')
-		const content = '---\n' +yamlhead_remove_n +'\n---\n' + __content_remove_n;
+		const __content_remove_n = __content.replace(/^\n/, "");
+		const content =
+			"---\n" + yamlhead_remove_n + "\n---\n" + __content_remove_n;
 		// const fullPath = this.getFilePath(nowFile);
 		//write content fo file
 		// fs.writeFileSync(fullPath, content);
 		try {
-			await nowFile.vault.modify(nowFile, content)
+			await nowFile.vault.modify(nowFile, content);
 		} catch (error) {
-			new Notice(`write file error ${error}`)
+			new Notice(`write file error ${error}`);
 		}
 	}
 }
--- main.ts
+++ main.ts
@@ -7,14 +7,13 @@ import {
 	Plugin,
 	PluginSettingTab,
 	Setting,
-	normalizePath
+	normalizePath,
 } from "obsidian";
 import { join } from "path";
 import * as fs from "fs";
-import {addIcons}  from 'icon';
+import { addIcons } from "icon";
 import { Upload2Notion } from "Upload2Notion";
-import {NoticeMConfig} from "Message";
-
+import { NoticeMConfig } from "Message";
 
 // Remember to rename these classes and interfaces!
 
@@ -26,7 +25,9 @@ interface PluginSettings {
 	proxy: string;
 }
 
-const langConfig =  NoticeMConfig( window.localStorage.getItem('language') || 'en')
+const langConfig = NoticeMConfig(
+	window.localStorage.getItem("language") || "en"
+);
 
 const DEFAULT_SETTINGS: PluginSettings = {
 	notionAPI: "",
@@ -40,7 +41,7 @@ export default class ObsidianSyncNotionPlugin extends Plugin {
 	settings: PluginSettings;
 	async onload() {
 		await this.loadSettings();
-		addIcons();	
+		addIcons();
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon(
 			"notion-logo",
@@ -59,40 +60,44 @@ export default class ObsidianSyncNotionPlugin extends Plugin {
 			id: "share-to-notion",
 			name: "share to notion",
 			editorCallback: async (editor: Editor, view: MarkdownView) => {
-				this.upload()
+				this.upload();
 			},
 		});
 
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
 	}
 
 	onunload() {}
 
-	async upload(){
+	async upload() {
 		const { notionAPI, databaseID } = this.settings;
-				if (notionAPI === "" || databaseID === "") {
-					new Notice(
-						"Please set up the notion API and database ID in the settings tab."
-					);
-					return;
-				}
-				const { markDownData, nowFile } =
-					await this.getNowFileMarkdownContent(this.app);
+		if (notionAPI === "" || databaseID === "") {
+			new Notice(
+				"Please set up the notion API and database ID in the settings tab."
+			);
+			return;
+		}
+		const { markDownData, nowFile } = await this.getNowFileMarkdownContent(
+			this.app
+		);
 
-				
-				if (markDownData) {
-					const { basename } = nowFile;
-					const upload = new Upload2Notion(this);
-					const res = await upload.syncMarkdownToNotion(basename, markDownData,nowFile, this.app, this.settings)
-					if(res.status === 200){
-						new Notice(`${langConfig["sync-success"]}${basename}`)
-					}else {
-						new Notice(`${langConfig["sync-fail"]}${basename}`)
-					}
-				}
+		if (markDownData) {
+			const { basename } = nowFile;
+			const upload = new Upload2Notion(this);
+			const res = await upload.syncMarkdownToNotion(
+				basename,
+				markDownData,
+				nowFile,
+				this.app,
+				this.settings
+			);
+			if (res.status === 200) {
+				new Notice(`${langConfig["sync-success"]}${basename}`);
+			} else {
+				new Notice(`${langConfig["sync-fail"]}${basename}`);
+			}
+		}
 	}
 
 	async getNowFileMarkdownContent(app: App) {
@@ -142,41 +147,40 @@ class SampleSettingTab extends PluginSettingTab {
 		const notionApiKye = new Setting(containerEl)
 			.setName("Notion API Token")
 			.setDesc("It's a secret")
-			.addText((text) =>{
+			.addText((text) => {
 				let t = text
-				.setPlaceholder("Enter your Notion API Token")
-				.setValue(this.plugin.settings.notionAPI)
-				.onChange(async (value) => {
-					this.plugin.settings.notionAPI = value;
-					await this.plugin.saveSettings();
-				})
-				t.inputEl.type = 'password'
-				return t
+					.setPlaceholder("Enter your Notion API Token")
+					.setValue(this.plugin.settings.notionAPI)
+					.onChange(async (value) => {
+						this.plugin.settings.notionAPI = value;
+						await this.plugin.saveSettings();
+					});
+				t.inputEl.type = "password";
+				return t;
 			});
-			
 
 		const notionDatabaseID = new Setting(containerEl)
 			.setName("Database ID")
 			.setDesc("It's a secret")
 			.addText((text) => {
 				let t = text
-				.setPlaceholder("Enter your Database ID")
-				.setValue(this.plugin.settings.databaseID)
-				.onChange(async (value) => {
-					this.plugin.settings.databaseID = value;
-					await this.plugin.saveSettings();
-				})
-				t.inputEl.type = 'password'
-				return t
-			}
-				
-			);
+					.setPlaceholder("Enter your Database ID")
+					.setValue(this.plugin.settings.databaseID)
+					.onChange(async (value) => {
+						this.plugin.settings.databaseID = value;
+						await this.plugin.saveSettings();
+					});
+				t.inputEl.type = "password";
+				return t;
+			});
 
-			notionDatabaseID.controlEl.querySelector('input').type='password'
-			
-			new Setting(containerEl)
+		notionDatabaseID.controlEl.querySelector("input").type = "password";
+
+		new Setting(containerEl)
 			.setName("Banner url(optional)")
-			.setDesc("page banner url(optional), default is empty, if you want to show a banner, please enter the url(like:https://raw.githubusercontent.com/EasyChris/obsidian-to-notion/ae7a9ac6cf427f3ca338a409ce6967ced9506f12/doc/2.png)")
+			.setDesc(
+				"page banner url(optional), default is empty, if you want to show a banner, please enter the url(like:https://raw.githubusercontent.com/EasyChris/obsidian-to-notion/ae7a9ac6cf427f3ca338a409ce6967ced9506f12/doc/2.png)"
+			)
 			.addText((text) =>
 				text
 					.setPlaceholder("Enter banner pic url: ")
@@ -187,10 +191,11 @@ class SampleSettingTab extends PluginSettingTab {
 					})
 			);
 
-
-			new Setting(containerEl)
+		new Setting(containerEl)
 			.setName("Notion ID(optional)")
-			.setDesc("Your notion ID(optional),share link likes:https://username.notion.site/,your notion id is [username]")
+			.setDesc(
+				"Your notion ID(optional),share link likes:https://username.notion.site/,your notion id is [username]"
+			)
 			.addText((text) =>
 				text
 					.setPlaceholder("Enter notion ID(options) ")
@@ -200,6 +205,5 @@ class SampleSettingTab extends PluginSettingTab {
 						await this.plugin.saveSettings();
 					})
 			);
-
 	}
 }
